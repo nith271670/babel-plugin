ac46c36e79d8a66e10606f052985361d597a20b5
nith271670 <nimisha.thilak@elektrobit.com>
2019-02-28 10:43:15 +0530

    label snippet

--- label_extractor.js
+++ label_extractor.js
@@ -1,19 +1,32 @@
+var path = require('path');
 const fs = require('fs');
-const { getTree } = require('./dist/jsx-to-simple-ast.cjs.js');
-var dir = __dirname + '/src/';
-var result = {};
-
-fs.writeFile('myfile.json', JSON.stringify(result, null, '\t'), (err) => {
-  if (err) throw err;
-  console.log(err);
-});
 var recursive = require('recursive-readdir-filter');
+var Parser = require('node-html-parser');
+var dir = directories + '/src/';
+
+var directories = path.dirname(__dirname); 
+
+
+if( path.basename(directories) == "node_modules"){
+  directories = path.dirname(directories);
+  dir = directories + '/src/';
+}
+else{
+  dir = __dirname + '/src/';
+}
+
+
+
+const getTree = (dir) => {
+var result = {};
+var finalTree = {};
+
 var options = {
   filterDir: function(stats) {
     return stats.name !== 'node_modules';
   },
   filterFile: function(stats) {
-    return stats.name.match(/\.js$/);
+    return stats.name.match(/\.tsx$ || \.js$ || \.ts$ || \.html$/);
   },
 };
 
@@ -22,20 +35,38 @@ recursive(dir, options, function(err, files) {
   files.forEach((file) => {
     const content = fs.readFileSync(file, 'utf8');
     if (file != undefined) {
-      if (getTree(content) != '') {
-        getTree(content).forEach((data) => {
-          Object.keys(data).forEach((key) => (result[key] = data[key]));
+      fs.readFile(file, 'utf8', function(err, contents) {
+        var root = Parser.parse(contents);
+        var label_obj = root.querySelectorAll('Label');
+       
+        label_obj.forEach((labelAttr) =>{
+        
+          if(labelAttr.attributes.uuid != undefined){
+            
+          var attrs = labelAttr.attributes;
+          var uuid = attrs.uuid;
+          delete attrs.uuid;
+          result[uuid] = attrs;
+         
+          }
+          
+          
         });
-        console.log(JSON.stringify(result, null, '\t'));
-        fs.writeFile(
-          'myfile.json',
-          JSON.stringify(result, null, '\t'),
-          (err) => {
-            if (err) throw err;
-            console.log(err);
-          },
-        );
-      }
+        finalTree = JSON.stringify(result, null, '\t');
+
+        fs.writeFile('labelList.json', JSON.stringify(result, null, '\t'), (err) => {
+          if (err) throw err;
+          console.log(err);
+        });
+        return finalTree;
+
+    });
+    
+
     }
   });
 });
+
+}
+
+console.log(getTree(dir));
---
+++ src/Welcome/abc/b/b.tsx
@@ -0,0 +1,18 @@
+export class Welcome {
+    return (
+      <>
+      <div>
+       <Label uuid="ss" description="desc4"/>
+       <div>
+      <Label uuid="abc" description="desc5"/>
+     </div>
+      </div>
+      <div>
+      <Label uuid="s1s" description="desc6"/>
+     </div>
+      </>
+      );
+    
+  };
+  
+  export default Welcome;
