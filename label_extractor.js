74eb6a50322ca59273bbfec609b10e536af6de1c
nith271670 <nimisha.thilak@elektrobit.com>
2019-03-01 14:47:09 +0530

    changed the dir path confg so that it doesn't conflict in project

--- label_extractor.js
+++ label_extractor.js
@@ -2,14 +2,16 @@ const { getTree } = require('./getTree');
 
 var path = require('path');
 
-var dir = path.resolve(path.resolve(__dirname));
+var dir = __dirname + '/src/';
 
-// if (path.basename(directories) == 'node_modules') {
-//   directories = path.dirname(directories);
-//   dir = directories + '/src/';
-// } else {
-//   dir = __dirname + '/src/';
-// }
-dir = dir + '/src/';
+var directories = path.dirname(__dirname);
+
+if (path.basename(directories) == 'node_modules') {
+   directories = path.dirname(directories);
+    dir = directories + '/src/';
+} else {
+  dir = __dirname + '/src/';
+}
+
 
 console.log(getTree(dir));
